daad511aa7427ec0e5637215199bb705e87f74b3
baluchandra1 <baluchandra1@gmail.com>
2023-12-17 10:24:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import matplotlib.pyplot as plt
+#from matplotlib import rc
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+
+
+#################SAMPLE SPACE#######################
+fig, ax = plt.subplots()
+######Function Range############
+xrange = [-10, 10]
+#yrange = [-100, 100]
+x = np.linspace(xrange[0], xrange[1], 200000)
+plt.axhline(y = 0, color = 'k', linestyle = '-')
+plt.axvline(x = 0, color = 'k', linestyle = '-')
+##########Function###########
+function = [[5*x**3,'$y=5x^3$'],[15*x**2,'$y=15x^2$'] ]
+#function_label = ['$y=x^2$', '$y=x^3$']
+
+def fctn(function, xrange):
+    i = len(function)
+    #x = np.linspace(xrange[0], xrange[1], 200000)
+    for y in function:
+        x = np.linspace(xrange[0], xrange[1], 200000)
+        ax.plot(x, y[0],label = y[1])
+
+        plt.legend()
+
+        #print(y)
+    return plt.show()
+plt.grid()
+fctn(function,xrange)
